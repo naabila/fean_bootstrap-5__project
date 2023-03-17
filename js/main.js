aa1710cc8f61f9244f3351786dbd4425c2a8ea08
Nabila Islam <nabilaislamhstu53@gmail.com>
2023-03-18 00:01:39 +0600

    home page done

--- js/main.js
+++ js/main.js
@@ -1,24 +1,32 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
+$(document).ready(function(){
+    var container = document.querySelector('.filter-container');
+    var mixer = mixitup(container, {
+        selectors: {
+            control: '[data-mixitup-control]'
+        }   
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout: 3000,
+        nav:true,
+        navText: ['<ion-icon name="arrow-back"></ion-icon>','<ion-icon name="arrow-forward"></ion-icon>'],
+        stagePadding:0,
+        margin:0,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:2
+            }
         }
-    }
+    })
 })
 
-//mixitup plugin
-const buttonContainer= document.querySelector('.filter-image-container');
-var mixer = mixitup(buttonContainer, {
-    selectors: {
-        control: '[data-ref="mix"]'
-    }
-});
+
+
